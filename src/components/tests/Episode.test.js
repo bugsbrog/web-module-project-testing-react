48c2d6ff359f2740a1206efacbbc7459f2a1a31f
bugsbrog <bugsbrog@gmail.com>
2021-12-13 12:50:06 -0700

    First episode test done

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,9 +3,24 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
+const episodeTest = {
+    id: 1,
+    image: "https://i.ibb.co/2FsfXqM/stranger-things.png",
+    name: "",
+    season: 1,
+    number: 1,
+    summary: "",
+    runtime: 1
+}
 
-test("renders without error", () => {});
+test("renders without error", () => {
+    render(<Episode episode ={episodeTest}/>)
+});
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders the summary test passed as prop", ()=> {
 
-test("renders default image when image is not defined", ()=>{});
+});
+
+test("renders default image when image is not defined", ()=> {
+
+});
