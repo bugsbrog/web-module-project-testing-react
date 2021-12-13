4779013fba1ddeea14bf874fc8753a88a0cc8474
bugsbrog <bugsbrog@gmail.com>
2021-12-13 14:08:03 -0700

    Test 2 passing and working on last one in Episode.test.js

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
@@ -9,18 +8,45 @@ const episodeTest = {
     name: "",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.",
+    runtime: 1
+}
+
+const episodeTestNoImage = {
+    id: 1,
+    image: "",
+    name: "",
+    season: 1,
+    number: 1,
+    summary: "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.",
     runtime: 1
 }
 
 test("renders without error", () => {
+    //Arrange
     render(<Episode episode ={episodeTest}/>)
 });
 
-test("renders the summary test passed as prop", ()=> {
+test("renders the summary test passed as prop", () => {
+    //Arrange
+    render(<Episode episode={episodeTest}/>)
 
+    //Act
+    const summary = screen.queryByText(/A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest./i);
+
+    //Assert
+    //Why won't .toBeInTheDocument or .toHaveTextContent pass?
+    expect(summary).not.toBeFalsy();
+    expect(summary).toBeTruthy();
+    expect(summary).not.toBeNull();
 });
 
-test("renders default image when image is not defined", ()=> {
+test("renders default image when image is not defined", () => {
+    //Arrange
+    render(<Episode episode={episodeTestNoImage}/>)
 
+    //Assert
+    const image = screen.queryByText('./stranger-things.png')
+
+    expect(image).toBeInTheDocument();
 });
