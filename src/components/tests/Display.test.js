602c49b10afbc4381ef12f3d20aba1d7a7c60ebe
bugsbrog <bugsbrog@gmail.com>
2021-12-13 15:51:21 -0700

    Test 1 passing for Display.test.js

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -6,12 +6,22 @@ import userEvent from '@testing-library/user-event';
 import Display from './../Display';
 
 
-test('renders without errors with no props', ()=>{
+test('renders without errors with no props', () => {
+    //Arrange
+    render(<Display/>);
+});
+
+test('renders Show component when the button is clicked ', () => {
+    //Arrange
 
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test('renders show season options matching your data when the button is clicked', () => {
+    //Arrange
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+});
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+test('renders show season options matching your data when the button is clicked', () => {
+    //Arrange
+
+});
